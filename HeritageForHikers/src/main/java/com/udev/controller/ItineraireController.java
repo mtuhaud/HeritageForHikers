5213bce8d8a814d07840c8a69831d4e70ba8ed8d
micka <micka@192.168.1.47>
2020-09-02 15:51:26 +0200

    ajout annotation CrossOrigin

--- HeritageForHikers/src/main/java/com/udev/controller/ItineraireController.java
+++ HeritageForHikers/src/main/java/com/udev/controller/ItineraireController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.udev.model.ItineraireDto;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ItineraireController {
 	
 	  @Autowired
